9addfe19f11579fefbe29433e5b1b6ff478e2d60
joe-katibi <katibilibuli@gmail.com>
2026-07-12 17:40:19 +0300

    resolving appointment_notificatin doesnt exist2

--- Modules/Appointment/database/migrations/2025_09_02_192838_create_appointment_status_histories_table.php
+++ Modules/Appointment/database/migrations/2025_09_02_192838_create_appointment_status_histories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAppointmentStatusHistoriesTable extends Migration
+class CreateAppointmentStatusHistoriesTableLegacy extends Migration
 {
   /**
    * Run the migrations.
