267ee65ae28d72be935dbb6394ac7ae1b8a4d9e3
joe-katibi <katibilibuli@gmail.com>
2026-07-12 17:54:06 +0300

    resolving appointment_notificatin doesnt exist4

--- database/migrations/2025_06_19_000000_create_escalations_table.php
+++ database/migrations/2025_06_19_000000_create_escalations_table.php
@@ -11,6 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
+      if (Schema::hasTable('escalations')) {
+        return;
+      }
+
       Schema::create('escalations', function (Blueprint $table) {
         $table->id();
         $table->integer('escalation_id')->nullable();
