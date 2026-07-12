12a7fc3ddaa532faabf7647f241e2b34d713fbbf
joe-katibi <katibilibuli@gmail.com>
2026-07-12 19:37:40 +0300

    Fix case sensitivity: rename Outages Routes directory to lowercase routes

--- Modules/Outages/routes/api.php
+++ Modules/Outages/routes/api.php
@@ -18,14 +18,14 @@ use Modules\Outages\Http\Controllers\OutageTicketController;
 Route::middleware('auth:api')->group(function () {
     // Outage API Endpoints
     Route::apiResource('outages', OutageController::class);
-    
+
     // Outage Ticket API Endpoints
     Route::apiResource('outage-tickets', OutageTicketController::class);
-    
+
     // Additional API endpoints can be added here
     Route::post('outages/{outage}/update-status', [OutageController::class, 'updateStatus']);
     Route::post('outage-tickets/{ticket}/update-progress', [OutageTicketController::class, 'updateProgress']);
-    
+
     // Dynamic team assignment endpoints
     Route::get('outages/team-types/{teamType}/sub-teams', [OutageController::class, 'getSubTeamTypes']);
     Route::get('outages/sub-team-types/{subTeamType}/users', [OutageController::class, 'getUsersBySubTeamType']);
