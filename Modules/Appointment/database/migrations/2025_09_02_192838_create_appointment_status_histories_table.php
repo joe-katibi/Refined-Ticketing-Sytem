223ca9e977c6fe42a2a4d9f37abff4d3c4d0932a
joe-katibi <katibilibuli@gmail.com>
2026-07-12 18:39:36 +0300

    resolving appointment tables1

--- Modules/Appointment/database/migrations/2025_09_02_192838_create_appointment_status_histories_table.php
+++ Modules/Appointment/database/migrations/2025_09_02_192838_create_appointment_status_histories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAppointmentStatusHistoriesTableLegacy extends Migration
+return new class extends Migration
 {
   /**
    * Run the migrations.
@@ -28,4 +28,4 @@ class CreateAppointmentStatusHistoriesTableLegacy extends Migration
   {
     Schema::dropIfExists('appointment_status_histories');
   }
-}
+};
